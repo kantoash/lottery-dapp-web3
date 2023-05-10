bce205a68cd363c81fdc8c4b114435f9d128b306
kantoash <kentoashketchum@gmail.com>
2023-05-10 15:46:40 +0530

    connectwallet

--- src/services/ssrblockchain.ts
+++ src/services/ssrblockchain.ts
@@ -10,7 +10,7 @@ if (typeof window !== "undefined") {
 }
 
 export const EthereumContract = async () => {
-  const provider = new ethers.providers.Web3Provider(ethereum);
+  const provider = new ethers.providers.JsonRpcProvider(process.env.DOMAIN_URL)
   const wallet = ethers.Wallet.createRandom();
   const signer = provider.getSigner(wallet.address);
   const Contract = new ethers.Contract(
